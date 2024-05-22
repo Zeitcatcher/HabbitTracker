b82da6a0c956781bcee3a96e60f24b3d65fcca8f
Zeitcatcher <ars.oksenoyt@gmail.com>
2024-05-21 16:51:32 -1000

    Initial Commit

---
+++ HabbitTracker/HabbitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabbitTrackerApp.swift
+//  HabbitTracker
+//
+//  Created by Arseniy Oksenoyt on 5/21/24.
+//
+
+import SwiftUI
+
+@main
+struct HabbitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
